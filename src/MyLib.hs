13030d4923183cff327531dceaa247aa150924c6
Tom Carstens <tom.carstens@hotmail.com>
2024-05-14 21:48:25 -0500

    implement macros as sugar for special forms

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -3,8 +3,8 @@ module MyLib where
 
 import Data.Scientific (Scientific)
 import qualified Data.Char as Char
-import Data.String (IsString(..))
-import Data.Text hiding (elem)
+import Data.String ()
+import Data.Text hiding (elem, foldl1, break, concat)
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -21,49 +21,89 @@ data MExpr
     -- ^ a string literal
     | Number Scientific
     -- ^ a numeric literal
-    | Compound [MExpr]
-    -- ^ a single item constructed from at least two parts, such as function application or an infix expression
     | Block [MExpr]
     -- ^ a collection of items which may be empty
+    | Compound [MExpr]
+    -- ^ a single item constructed from at least two parts, such as function application or an infix expression
+    -- v types of compound made via reconstruct
+    | Apply MExpr MExpr
+    | Binary MExpr MExpr MExpr -- infix 1 + 2
     deriving (Eq, Ord, Show)
 
+sc :: Parser ()
 sc = L.space hspace1 (L.skipLineComment "//") (L.skipBlockCommentNested "/*" "*/")
+scn :: Parser ()
 scn = L.space space1 (L.skipLineComment "//") (L.skipBlockCommentNested "/*" "*/")
+lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
+symbol :: Text -> Parser Text
 symbol = L.symbol sc
+
 number :: Parser MExpr
 number = Number <$> L.signed (pure ()) (lexeme L.scientific)
+
 lit :: Parser MExpr
 lit =
     String . pack
     <$> lexeme ((char '"' *> manyTill L.charLiteral (char '"'))
     <|> (char '\'' *> manyTill L.charLiteral (char '\'')))
+
 atom :: Parser MExpr
 atom = Atom . pack <$> lexeme (some p)
     where p = satisfy (\c -> not (c `elem` (";[]{}()"::String) || Char.isSpace c))
+
+operator :: Parser MExpr
 operator = Operator . pack <$> lexeme (some p)
     where p = satisfy (\c -> c /= ';' && (Char.isSymbol c || Char.generalCategory c `elem` [Char.ConnectorPunctuation, Char.DashPunctuation, Char.OtherPunctuation])) <?> "operator"
 
 block :: Parser MExpr
 block =
     let body = sepEndBy expr (symbol ";")
-        d (op,cl) = Block <$> between op cl body
-        i (op,cl) = Block <$> between op cl (many (expr <* optional (symbol ";")))
-        brackets = (\ (a,b) -> (symbol a, symbol b)) <$> [("[","]"), ("{","}"), ("(",")")]
-        delim = choice (fmap d brackets)
-        indent = choice (fmap i brackets)
-    in try delim <|> indent
-
-simple = try number <|> try lit <|> try operator <|> atom <|> block
+        d (f,op,cl) = f <$> between op cl body
+        comp [x] = x
+        comp xs = Compound xs
+        brackets = (\ (f,a,b) -> (f,symbol a, symbol b)) <$> [(Block,"[","]"), (Block,"{","}"), (comp, "(",")")]
+    in choice (fmap d brackets)
 
 expr :: Parser MExpr
-expr = L.lineFold scn $ \sc' ->
-  let
-    ps :: Parser [MExpr] 
-    ps = simple `sepBy1` try sc'
+expr = convert <$> ps
+  where
+    ps = L.lineFold scn $ \sc' ->
+        concat <$> (some simple `sepBy1` try sc') <* scn
+    simple = try number <|> try lit <|> try operator <|> atom <|> block
     convert [x] = x
     convert xs = Compound xs
-  in convert <$> (ps <* scn) -- (1)
-top = many expr
-parse str = runParser (top <* eof) "" str 
 
+parse :: Text -> Either String [MExpr]
+parse str = 
+    case runParser (many (L.nonIndented scn expr) <* eof) "" str of
+        Right e -> Right e
+        Left err -> Left (errorBundlePretty err)
+
+reconstruct :: ((MExpr -> Either a MExpr) -> [MExpr] -> Maybe MExpr) -> MExpr -> Either a MExpr
+reconstruct isSpecial e =
+    case e of
+        Block es -> Block <$> traverse (reconstruct isSpecial) es
+        Compound es -> findOperator <$> traverse (reconstruct isSpecial) es
+        _ -> Right e
+    where
+        isOperator (Operator _) = True
+        isOperator _ = False
+        findOperator es =
+            case isSpecial (reconstruct isSpecial) es of
+                Nothing ->
+                    case break isOperator es of
+                        (xs, op: ys) -> Binary (foldl1 Apply xs) op (findOperator ys)
+                        _ -> foldl1 Apply es
+                Just special -> special
+
+macros :: (MExpr -> Either a MExpr) -> [MExpr] -> Maybe MExpr
+macros _ [Atom "let", x, Operator "=", y] = Just (Apply (Atom "let") (Block [Binary x (Operator "=") y]))
+macros k ((Atom "let"):x:(Operator "="):rest) =
+    case k (Compound rest) of
+        Right def -> Just (Apply (Atom "let") (Block [Binary x (Operator "=") def]))
+        Left _ -> Nothing
+macros _ _ = Nothing
+
+macroExpand :: MExpr -> Either a MExpr
+macroExpand = reconstruct macros
--- test/Main.hs
+++ test/Main.hs
@@ -13,17 +13,106 @@ main = hspec $ do
             parse "[1;2;3]" `shouldBe` Right [Block [Number 1, Number 2, Number 3]]
         it "nested block" $ do
             parse "[[];[[]]]" `shouldBe` Right [Block [Block[], Block[Block[]]]]
-        it "block with no semicolons" $ do
-            parse "[\n\t1\n\t2\n\t3]" `shouldBe` Right [Block[Number 1, Number 2, Number 3]]
-        it "block indented optional semicolons" $ do
-            parse "[\n\t1\n\t2;3;4\n\t5]" `shouldBe` Right [Block[Number 1, Number 2, Number 3, Number 4, Number 5]]
-            parse "[1;2;3;4\n5]" `shouldBe` Right [Block[Number 1, Number 2, Number 3, Number 4, Number 5]]
         it "atom" $ do
             parse "foo" `shouldBe` Right [Atom "foo"]
-        it "Compounduences simple" $ do
-            parse "a b c" `shouldBe` Right [Compound [Atom "a", Atom "b", Atom "c"]]
-        it "Compounduences multiline" $ do
-            parse "a\n b\n c\nd\ne\n f\n  g" `shouldBe` Right [Compound [Atom "a",Atom "b",Atom "c"],Atom "d",Compound [Atom "e",Atom "f",Atom "g"]]
+        it "Compounds simple" $ do
+            let mexpr = parse "a b c" 
+            mexpr `shouldBe` Right [Compound [Atom "a", Atom "b", Atom "c"]]
+            (mexpr >>= traverse macroExpand) `shouldBe` Right [Apply (Apply (Atom "a") (Atom "b")) (Atom "c")]
+        it "Compounds with blocks" $ do
+            let mexpr = parse "f [g a]"
+            mexpr `shouldBe` Right [Compound[Atom "f", Block [Compound[Atom "g", Atom "a"]]]]
+            (mexpr >>= traverse macroExpand) `shouldBe` Right [Apply (Atom "f") (Block[Apply (Atom "g") (Atom "a")])]
+        it "Compounds with operators" $ do
+            let mexpr = parse "f a + g b + h c"
+            mexpr `shouldBe` Right [Compound[Atom "f", Atom "a", Operator "+", Atom "g", Atom "b", Operator "+", Atom "h", Atom "c"]]
+            (mexpr >>= traverse macroExpand) `shouldBe` Right [Binary (Apply (Atom "f") (Atom "a")) (Operator "+") (Binary (Apply (Atom "g") (Atom "b")) (Operator "+") (Apply (Atom "h") (Atom "c")))]
     describe "Realistic Parsing" $ do
+        it "multiple statements" $ do
+            let mexpr = parse "let x = 1\nlet y = 2\nx + y"
+            mexpr `shouldBe` Right [Compound[Atom "let", Atom "x", Operator "=", Number 1], Compound[Atom "let", Atom "y", Operator "=", Number 2], Compound[Atom "x", Operator "+", Atom "y"]]
+            (mexpr >>= traverse macroExpand) `shouldBe` Right [Apply (Atom "let") (Block [Binary (Atom "x") (Operator "=") (Number 1)]), Apply (Atom "let") (Block [Binary (Atom "y") (Operator "=") (Number 2)]), Binary (Atom "x") (Operator "+") (Atom "y")]
+        it "increment" $ do
+            let mexpr = parse "let incr = x -> x + 1"
+            mexpr `shouldBe` Right [Compound[Atom "let", Atom "incr", Operator "=", Atom "x", Operator "->", Atom "x", Operator "+", Number 1]]
+            (mexpr >>= traverse macroExpand) `shouldBe` Right [Apply (Atom "let") (Block [Binary (Atom "incr") (Operator "=") (Binary (Atom "x") (Operator "->") (Binary (Atom "x") (Operator "+") (Number 1)))])]
         it "factorial" $ do
-            parse "let factorial = {\n\t1 -> 1\n\tx -> x * factorial (x - 1)}" `shouldBe` Right [Compound [Atom "let", Atom "factorial", Operator "=", Block [Compound [Number 1, Operator "->", Number 1], Compound [Atom "x", Operator "->", Atom "x", Operator "*", Atom "factorial", Block [Compound [Atom "x", Operator "-", Number 1]]]]]]
+            let mexpr = parse "let factorial = {\n\t0 -> 1;\n\tx -> x * factorial (x - 1)}"
+            mexpr `shouldBe` Right [Compound [Atom "let", Atom "factorial", Operator "=", Block [Compound [Number 0, Operator "->", Number 1], Compound [Atom "x", Operator "->", Atom "x", Operator "*", Atom "factorial", Compound [Atom "x", Operator "-", Number 1]]]]]
+            (mexpr >>= traverse macroExpand) `shouldBe` Right 
+                [ Apply 
+                    (Atom "let") 
+                    (Block 
+                        [ Binary 
+                            (Atom "factorial") 
+                            (Operator "=") 
+                            (Block 
+                                [ Binary
+                                    (Number 0.0) 
+                                    (Operator "->") 
+                                    (Number 1.0)
+                                , Binary 
+                                    (Atom "x") 
+                                    (Operator "->") 
+                                    (Binary 
+                                        (Atom "x") 
+                                        (Operator "*") 
+                                        (Apply 
+                                            (Atom "factorial") 
+                                            (Binary 
+                                                (Atom "x") 
+                                                (Operator "-") 
+                                                (Number 1.0))))
+                                ])
+                        ])
+                ]
+        it "mutual recursion" $ do
+            let mexpr = parse "let [isOdd = { 1 -> true; x -> not (isEven (x - 1))}; isEven = {0 -> true; x -> not (isOdd (x - 1))}]"
+            mexpr `shouldBe` Right [Compound [Atom "let",Block [Compound [Atom "isOdd",Operator "=",Block [Compound [Number 1.0,Operator "->",Atom "true"],Compound [Atom "x",Operator "->",Atom "not",Compound [Atom "isEven",Compound [Atom "x",Operator "-",Number 1.0]]]]],Compound [Atom "isEven",Operator "=",Block [Compound [Number 0.0,Operator "->",Atom "true"],Compound [Atom "x",Operator "->",Atom "not",Compound [Atom "isOdd",Compound [Atom "x",Operator "-",Number 1.0]]]]]]]]
+            (mexpr >>= traverse macroExpand) `shouldBe` Right 
+                [ Apply 
+                    (Atom "let") 
+                    (Block 
+                        [ Binary 
+                            (Atom "isOdd") 
+                            (Operator "=") 
+                            (Block 
+                                [ Binary 
+                                    (Number 1.0) 
+                                    (Operator "->") 
+                                    (Atom "true")
+                                , Binary 
+                                    (Atom "x") 
+                                    (Operator "->") 
+                                    (Apply 
+                                        (Atom "not") 
+                                        (Apply 
+                                            (Atom "isEven") 
+                                            (Binary 
+                                                (Atom "x") 
+                                                (Operator "-") 
+                                                (Number 1.0))))
+                                ])
+                        , Binary 
+                            (Atom "isEven") 
+                            (Operator "=") 
+                            (Block 
+                                [ Binary 
+                                    (Number 0.0) 
+                                    (Operator "->") 
+                                    (Atom "true")
+                                , Binary 
+                                    (Atom "x") 
+                                    (Operator "->")
+                                    (Apply 
+                                        (Atom "not") 
+                                        (Apply 
+                                            (Atom "isOdd") 
+                                            (Binary 
+                                                (Atom "x") 
+                                                (Operator "-") 
+                                                (Number 1.0))))
+                                ])
+                        ])
+
+                ]
